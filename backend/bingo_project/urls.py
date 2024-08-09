e811e9271231b26eba5a7a14df4cf3cf596a518e
FrancoArriola7 <113956731+FrancoArriola7@users.noreply.github.com>
2024-08-09 01:49:16 -0300

    Update urls.py redirect imported

--- backend/bingo_project/urls.py
+++ backend/bingo_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect('/api/')
 
 urlpatterns = [
+    path('', home_redirect),  # Redirige la raíz a /api/
     path('admin/', admin.site.urls),
-    path('api/', include('bingo.urls'))
+    path('api/', include('bingo.urls')),
 ]
